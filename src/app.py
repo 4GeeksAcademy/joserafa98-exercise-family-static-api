849809a04b69af13f271571f318a9cee649a0d91
Jose Hernandez <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-09-18 16:38:52 +0000

    first version of my API family

--- src/app.py
+++ src/app.py
@@ -38,6 +38,25 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member = request.get_json()
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
